d6f8c8d19fd820c856fa3ad4fb7b37844b1f90e1
Yusuff Rasheed <yusuffrasheed.yr@gmail.com>
2025-08-07 20:02:40 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
+    allowedHosts: ['delete-account-1adm.onrender.com'],
     host: true,
     port: process.env.PORT || 4173,
     strictPort: true,
